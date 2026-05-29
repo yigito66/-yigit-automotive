c5f0502c3340041378238a4aef1356273be1a1af
yigito66 <yigitsunaaa@gmail.com>
2026-05-30 00:35:55 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+const cars = [
+  { id: 1, brand: "BMW", model: "i5", hp: 340, topSpeed: 193 },
+  { id: 2, brand: "Tesla", model: "Model 3", hp: 513, topSpeed: 261 },
+  { id: 3, brand: "Audi", model: "A6", hp: 265, topSpeed: 250 }
+];
