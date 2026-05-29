256f07716ef3cd1912a90f22c855b2ca4c2c5d92
yigito66 <yigitsunaaa@gmail.com>
2026-05-30 00:41:17 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,20 @@
-const cars = [
-  { id: 1, brand: "BMW", model: "i5", hp: 340, topSpeed: 193 },
-  { id: 2, brand: "Tesla", model: "Model 3", hp: 513, topSpeed: 261 },
-  { id: 3, brand: "Audi", model: "A6", hp: 265, topSpeed: 250 }
-];
+const brands = ["BMW", "Audi", "Mercedes", "Tesla", "Toyota", "Ford", "Hyundai", "Kia", "Porsche", "Volkswagen"];
+const models = ["X", "M", "S", "GT", "RS", "Sport", "Pro", "EV", "Turbo", "Line"];
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// 🚗 1000 araba üret
+const cars = Array.from({ length: 1000 }, (_, i) => {
+  const brand = brands[random(0, brands.length - 1)];
+  const model = models[random(0, models.length - 1)] + " " + random(1, 99);
+
+  return {
+    id: i + 1,
+    brand,
+    model,
+    hp: random(90, 1000),
+    topSpeed: random(160, 350)
+  };
+});
